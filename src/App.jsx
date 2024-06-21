3c74310b9cde6e80d524bacbf6fea6b27b8dbf7a
AmritShandilya12 <amrit.shandilya@tigeranalytics.com>
2024-06-21 15:28:51 +0530

    Reducer hook1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import Countertwo from './Components/Countertwo'
+import React, { useReducer } from 'react'
+import ComponentA from './Components/ComponentA'
+import ComponentB from './Components/ComponentB'
+import ComponentC from './Components/ComponentC'
 
+export const CountContext=React.createContext()
 
+const initialState=0
+const reducer=(state,action)=>{
+    switch(action){
+        case 'increment':
+            return state+1
+        case 'decrement':
+            return state-1
+        case 'reset':
+            return initialState
+        default:
+            return state
+
+    }
+}
 
 function App() {
 
-
+const[count,dispatch]=useReducer(reducer,initialState)
   return (
-    <>
+    <CountContext.Provider value={{countState:count,countDispatch:dispatch}}>
     {/* <HookMouse/> */}
     {/* <HookCounterOne/> */}
     {/* <MouseContainer/> */}
     {/* <IntervalHook/> */}
     {/* <DataFetching/> */}
     {/* <CounterOne/> */}
-    <Countertwo/>
-    </>
+    {/* <Countertwo/> */}
+    {/* <CounterThree/> */}
+    <div>count- {count}</div>
+    <ComponentA/>
+    <ComponentB/>
+    <ComponentC/>
+    </CountContext.Provider>
   )
 }
 
